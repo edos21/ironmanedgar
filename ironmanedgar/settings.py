13594c23f28f5962619aaa97fa217096c46cf590
edos21 <edos21@gmail.com>
2018-04-02 17:27:09 -0300

    remove pdb middleware

--- ironmanedgar/settings.py
+++ ironmanedgar/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_pdb.middleware.PdbMiddleware',
 ]
 
 ROOT_URLCONF = 'ironmanedgar.urls'
