a9f020e64e0ad22b0dc902d02db78cf64b101f9b
edos21 <edos21@gmail.com>
2018-04-23 17:45:32 -0300

    upload fast send_mail view

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
 def index(request):
     return render(request, 'home.html')
+
+def contact(request):
+    subject = request.POST.get('subject', '')
+    message = request.POST.get('message', '')
+    from_email = request.POST.get('email', '')
+    if subject and message and from_email:
+        try:
+            send_mail(subject, message, from_email, ['edos21@gmail.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return HttpResponseRedirect('/contact/thanks/')
+    else:
+        # In reality we'd use a form class
+        # to get proper validation errors.
+        return HttpResponse('Make sure all fields are entered and valid.')
