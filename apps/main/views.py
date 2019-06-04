98c8584d783043378a92296b892ea6be565f1a17
edos21 <edos21@gmail.com>
2019-06-04 12:16:31 -0300

    adds mail recipient

--- apps/main/views.py
+++ apps/main/views.py
@@ -10,11 +10,22 @@ def index(request):
 
 def contact(request):
     subject = request.POST.get('subject', '')
+    name = request.POST.get('name', '')
     message = request.POST.get('message', '')
     from_email = request.POST.get('email', '')
     if subject and message and from_email:
+        html_body = (
+            "<h3>Mensaje desde EL-Electric.net</h3>"
+            "<hr>"
+            "<h2>" + name + " - " + from_email + "</h2>"
+            "<h1>" + subject + "</h1>"
+            "<hr>"
+            "<p>" + message + "</p>"
+        )
         try:
-            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, ['edos21@gmail.com'])
+            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL,
+                ['edos21@gmail.com', "edgarironman@gmail.com"], html_message=html_body
+            )
         except BadHeaderError:
             return HttpResponse('Invalid header found.')
         return HttpResponseRedirect('/?send-mail')
